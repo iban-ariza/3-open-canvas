378f81764b02234b1029835cf02880f36f3ea92f
Iban Ariza <iban.ariza@outlook.com>
2025-03-02 16:53:46 -0500

    comment(ArtifactHeader): Header component with title, saved, and navigation buttons
    This component is where the artifact/document/canvas title, saved, and
    navigation buttons (forward/backward) buttons are.

--- apps/web/src/components/artifacts/header/index.tsx
+++ apps/web/src/components/artifacts/header/index.tsx
@@ -1,11 +1,13 @@
+import { TooltipIconButton } from "@/components/ui/assistant-ui/tooltip-icon-button";
+import { Assistant } from "@langchain/langgraph-sdk";
+import { ArtifactCodeV3, ArtifactMarkdownV3 } from "@opencanvas/shared/types";
+import { PanelRightClose } from "lucide-react";
 import { ReflectionsDialog } from "../../reflections-dialog/ReflectionsDialog";
 import { ArtifactTitle } from "./artifact-title";
 import { NavigateArtifactHistory } from "./navigate-artifact-history";
-import { ArtifactCodeV3, ArtifactMarkdownV3 } from "@opencanvas/shared/types";
-import { Assistant } from "@langchain/langgraph-sdk";
-import { PanelRightClose } from "lucide-react";
-import { TooltipIconButton } from "@/components/ui/assistant-ui/tooltip-icon-button";
 
+// Most of the imports are for 'types' for the props.
+// TODO - consider removing so many props and using context
 interface ArtifactHeaderProps {
   isBackwardsDisabled: boolean;
   isForwardDisabled: boolean;
@@ -19,9 +21,15 @@ interface ArtifactHeaderProps {
   setChatCollapsed: (c: boolean) => void;
 }
 
+/*
+ArtifactHeader - is the component that encapsulates the whole right pane. It is the document. And it is composed of:
+- ArtifactTitle
+- NavigateArtifactHistory (backward/forward buttons)
+*/
 export function ArtifactHeader(props: ArtifactHeaderProps) {
   return (
     <div className="flex flex-row items-center justify-between">
+      {/* ARTIFACT TITLE CONTAINER */}
       <div className="flex flex-row items-center justify-center gap-2">
         {props.chatCollapsed && (
           <TooltipIconButton
@@ -34,13 +42,16 @@ export function ArtifactHeader(props: ArtifactHeaderProps) {
             <PanelRightClose className="text-gray-600" />
           </TooltipIconButton>
         )}
+        {/* ARTIFACT/DOCUMENT TITLE */}
         <ArtifactTitle
           title={props.currentArtifactContent.title}
           isArtifactSaved={props.isArtifactSaved}
           artifactUpdateFailed={props.artifactUpdateFailed}
         />
       </div>
+      {/* NAVIGATION FORWARD/BACKWARD CONTAINER */}
       <div className="flex gap-2 items-end mt-[10px] mr-[6px]">
+        {/* NAVIGATION (FORWARD/BACKWARD) BUTTONS */}
         <NavigateArtifactHistory
           isBackwardsDisabled={props.isBackwardsDisabled}
           isForwardDisabled={props.isForwardDisabled}
