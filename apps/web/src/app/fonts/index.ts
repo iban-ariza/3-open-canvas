0e9e04d36beaa928498ecf585703744cb3a49d61
Iban Ariza <iban.ariza@outlook.com>
2025-03-02 20:21:52 -0500

    install(fonts): Arboria bold and book fonts

---
+++ apps/web/src/app/fonts/index.ts
@@ -0,0 +1,13 @@
+import localFont from 'next/font/local';
+
+export const arboriaBold = localFont({
+    src: './Arboria-Bold.woff2',
+    variable: '--font-arboria-bold',
+    display: 'swap',
+});
+
+export const arboriaBook = localFont({
+    src: './Arboria-Book.woff2',
+    variable: '--font-arboria-book',
+    display: 'swap',
+});
